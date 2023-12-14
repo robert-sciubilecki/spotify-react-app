5ae3ed494da90ef490bc20c14f629c61e06aa256
sciubilecki <sciubilecki.robert@gmail.com>
2023-12-14 21:26:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <>
+      <h1 className={styles.header}>Hello</h1>
+    </>
+  );
+}
+
+export default App;
